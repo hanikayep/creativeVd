6e3268093e13e363ffacd890a4e6a9c471603212
Hannah Oliveros <email>
2023-03-02 10:06:07 +0800

    toggle menu

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const nav = document.querySelector(".nav");
+const toggleMenu = document.querySelector(".toggle__menu");
+
+
+toggleMenu.addEventListener("click", ()=>{
+    toggleMenu.classList.toggle("open");
+    nav.classList.toggle("open");
+})
